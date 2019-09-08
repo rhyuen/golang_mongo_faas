a722add01bcab660562e6519c041a0d4afc0ccee
robert <dungeonry@hotmail.com>
2019-09-08 14:23:51 -0700

    No func main() in root and types.go has its own folder. an attempt.

--- functions/updateQuote.go
+++ functions/updateQuote.go
@@ -1,10 +1,15 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/rhyuen/golang_mongo_faas/types"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "update endpoint")
+	cap := types.Quote{"Captain America", "I can do this all day."}
+	json.NewEncoder(w).Encode(cap)
 }
