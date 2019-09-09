b494a0493c4c7c49fc0fd37f6aaa5862028924b2
robert <dungeonry@hotmail.com>
2019-09-09 03:00:38 -0700

    middleware thing that i am attempting.

---
+++ mw/middleware.go
@@ -0,0 +1,28 @@
+package mw
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBConnect() (*mongo.Client, error) {
+	url := os.Getenv("go_mongo_db")
+	clientOptions := options.Client().ApplyURI(url).SetRetryWrites(false)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	fmt.Println("InitDB connected to MongoDB.")
+
+	return client, nil
+}
